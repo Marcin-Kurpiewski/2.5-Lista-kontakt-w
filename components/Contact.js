98906201670d6080023833929682d04d37e0d011
Marcin-Kurpiewski <kurus.mk@gmail.com>
2017-06-22 20:30:02 +0200

    add styles

--- components/Contact.js
+++ components/Contact.js
@@ -6,11 +6,11 @@ var Contact = React.createClass({
 
 	render: function(){
 		return (
-			React.createElement( 'div', { className: ' contactItem'},
+			React.createElement( 'div', { className: ' contactItem', },
 				React.createElement( 'img', { className: ' contactImage', src:'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}),
 				React.createElement( 'p', { className:'contactLabel'}, "Imię: " + this.props.item.firstName),
 				React.createElement( 'p', { className: 'contactLabel'}, "Nazwisko: " + this.props.item.lastName),
-				React.createElement( 'a', { className: 'contactEmail', href:'mailto:' + this.props.item.email}, this.props.item.email)
+				React.createElement( 'a', { className: 'contactEmail', href:'mailto:' + this.props.item.email}, "Email: " + this.props.item.email)
 			)
 		)
 	},
